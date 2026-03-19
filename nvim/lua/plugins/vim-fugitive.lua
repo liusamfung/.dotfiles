10f5e8a5a8274367901ee8744479ecc9111cf1db
liusamfung <liusam_dev@outlook.com>
2026-03-19 17:47:49 -0500

    feat(tools): configure fugitive and yazi integration

--- nvim/lua/plugins/vim-fugitive.lua
+++ nvim/lua/plugins/vim-fugitive.lua
@@ -1,6 +1,8 @@
 return {
-  {
-    'tpope/vim-fugitive',
-    -- :Git blame -w to show "What did I do here?? in commits"
-  },
+	--TODO: investigar que como usar este plugin.
+	{
+		"tpope/vim-fugitive",
+		cmd = { "Git", "G", "Gdiffsplit", "Gread", "Gwrite", "Ggrep", "GMove", "GDelete", "GBrowse" },
+		-- :Git blame -w to show "What did I do here?? in commits"
+	},
 }
--- nvim/lua/plugins/yazi.lua
+++ nvim/lua/plugins/yazi.lua
@@ -1,6 +1,7 @@
 return {
 	{
 		"mikavilpas/yazi.nvim",
+		-- event = "VeryLazy",
 		dependencies = {
 			-- check the installation instructions at
 			-- https://github.com/folke/snacks.nvim
