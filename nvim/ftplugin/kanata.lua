720e86e9eae512543c21306e294dba54fdfa8016
liusamfung <liusam_dev@outlook.com>
2026-03-18 21:41:03 -0500

    kanat with escape and backspace in pulgar fingers

---
+++ nvim/ftplugin/kanata.lua
@@ -0,0 +1,5 @@
+-- vim.treesitter.language.register("<parser name>", "<filetype name")
+vim.treesitter.language.register("clojure", "kanata")
+vim.opt_local.commentstring = ";; %s"
+vim.opt_local.shiftwidth = 4
+vim.opt_local.tabstop = 4
